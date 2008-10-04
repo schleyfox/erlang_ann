d4423b92a2cd22154e26ad7e8ca1a11aa886d072
Ben Hughes <ben@pixelmachine.org>
2008-10-04 13:19:29 -0400

    Summary: Create methods to hook together perceptrons
    Neural nets are networks of connected perceptrons.  They can be very
    naturally represented by directed graphs.  These weighted connections
    are the backbone of the contraption.
    
    My first priority for creating a server representation is thus to
    create a mechanism for connecting perceptrons to each other.  This
    will be done through gen_server casts.
    
    gen_server has two methods for communicating with the server, casts
    and calls.  Casts are asynchronous and do not return a reply.  Calls
    are synchronous and return a reply.  Calls basically act like normal
    function calls, except against a server.
    
    For connecting perceptrons together, I am using a cast because the
    only possible return value would be an 'ok' confirmation.  Connecting
    perceptrons should always work unless something goes horribly wrong,
    in which case throwing an exception, failing, and burning that mother
    down are the appropriate responses.
    
    The input and output connections are the beginning of state for our
    server, so we must update the state record to represent this.  I give
    inputs and outputs a default value of an empty list.  Each element in
    either of those lists represents a weighted connection, so I further
    define a connection record which consists of the PID address of the
    connected node and the weight.  All connections are started at the
    weight of 0.5.
    
    The actual magic happens in the handle_cast callbacks.  I define
    function clauses that match the messages {connect_to_input, PID} and
    {connect_to_output, PID}.  These then extract either inputs or outputs
    from the server state, copy the state record, changing the appropriate
    list to contain the new connection.
    
    The final step for this commit is to write a connect API method that
    abstracts this tomfoolery from the user. It takes two nodes, A and B,
    and connects B as an output to A and A as an input to B.

--- perceptron.erl
+++ perceptron.erl
@@ -4,18 +4,24 @@
 -behaviour(gen_server).
 
 % API
--export([start_link/0]).
+-export([start_link/0, connect/2]).
 
 % gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
     code_change/3]).
 
--record(state, {}).
+-record(state, {inputs=[], outputs=[]}).
+-record(connection, {node, weight=0.5}).
 
 % API
 
 % Starts a server
 start_link() -> gen_server:start_link(?MODULE, [], []).
+
+% connects A to B where A and B are PIDs of perceptrons
+connect(A, B) ->
+  gen_server:cast(A, {connect_to_output, B}),
+  gen_server:cast(B, {connect_to_input, A}).
 
 
 % gen_server callbacks
@@ -26,7 +32,17 @@ init([]) ->
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
+
+handle_cast({connect_to_input, PID}, #state{inputs=Inputs} = State) ->
+  {noreply,
+    State#state{inputs = [#connection{node=PID}|Inputs]}};
+handle_cast({connect_to_output, PID}, #state{outputs=Outputs} = State) ->
+  {noreply,
+    State#state{outputs = [#connection{node=PID}|Outputs]}};
 handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
   {noreply, State}.
 
 terminate(_Reason, _State) ->
