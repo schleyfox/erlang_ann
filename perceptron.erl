ee77d0142025978be9e301e6ee0817979416dc8b
Ben Hughes <ben@pixelmachine.org>
2008-10-04 14:52:38 -0400

    Summary: Implement basic feed-forward messages
    The actual operation of a feed-forward neural network is fairly
    simple.  A perceptron is stimulated by the output values of its input
    nodes, the dot product of the weights and input values is taken, the
    dot product is adjusted by a thresholding function (in this case a
    simple sigmoid), and is then passed on as stimulation to the
    perceptron's outputs.
    
    To implement this, I first have to modify the connections record to
    contain a field for the value emitted by the node.  This is only
    relevant for input connections, but maintaining a default value (0)
    will not affect output connections. The stimulation is handled through
    the {stimulate, From, Value} cast.  The inputs are updated with the
    new value (update_inputs/3).  The output value is then calculated
    (output_value/1).  The output value is then passed on to the output
    connections (stimulate_outputs/2).  If the node is terminal, then the
    output is written to the shell (for now).
    
    I also add a stimulate method to the API.  This is actually used in
    stimulate_outputs to handle the actual message passing.
    
    You might notice that this causes a recomputation of the output value
    and restimulation of all connection values whenever a single input is
    updated.  The proper behavior is to wait for all inputs to be updated.
    This will be handled in a later installment.
    
    Hopefully the code is fairly understandable at this stage.  I did not
    mention all of the details of how everything works, but I hope that a
    reading of the code will yield understanding.

--- perceptron.erl
+++ perceptron.erl
@@ -4,14 +4,14 @@
 -behaviour(gen_server).
 
 % API
--export([start_link/0, connect/2]).
+-export([start_link/0, connect/2, stimulate/2]).
 
 % gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
     code_change/3]).
 
 -record(state, {inputs=[], outputs=[]}).
--record(connection, {node, weight=0.5}).
+-record(connection, {node, weight=0.5, value=0}).
 
 % API
 
@@ -23,6 +23,9 @@ connect(A, B) ->
   gen_server:cast(A, {connect_to_output, B}),
   gen_server:cast(B, {connect_to_input, A}).
 
+stimulate(Node, Value) ->
+  gen_server:cast(Node, {stimulate, self(), Value}).
+
 
 % gen_server callbacks
 
@@ -32,7 +35,12 @@ init([]) ->
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
+handle_cast({stimulate, From, Value}, 
+  #state{inputs=Inputs, outputs=Outputs} = State) ->
 
+  New_Inputs = update_inputs(Inputs, From, Value),
+  stimulate_outputs(Outputs, output_value(New_Inputs)),
+  {noreply, State#state{inputs=New_Inputs}};
 handle_cast({connect_to_input, PID}, #state{inputs=Inputs} = State) ->
   {noreply,
     State#state{inputs = [#connection{node=PID}|Inputs]}};
@@ -50,3 +58,67 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
+
+% Internal Functions
+
+% update the value of the input connection referenced by the node's PID
+% in the list of inputs
+update_inputs(Inputs, Node, New_Value) ->
+  lists:map(
+    fun(#connection{node=N} = C) ->
+      case N of
+        Node -> C#connection{value=New_Value};
+        _ -> C
+      end
+    end,
+    Inputs).
+
+% outputs the output value to the terminal
+stimulate_outputs([], Output_Value) ->
+  io:format("~w: ~w~n", [self(), Output_Value]);
+% passes on the output value to the output connections
+stimulate_outputs(Outputs, Output_Value) ->
+  lists:foreach(
+    fun(PID) ->
+      stimulate(PID, Output_Value)
+    end,
+    perceptron_nodes(Outputs)).
+
+% returns a list of node PIDs from a list of connections
+perceptron_nodes(Connections) ->
+  lists:map(
+    fun(#connection{node=N}) -> N end,
+    Connections).
+
+% returns a weight vector from a list of connections
+weights(Connections) ->
+  lists:map(
+    fun(#connection{weight=W}) -> W end,
+    Connections).
+
+% returns a value vector from a list of connections
+values(Connections) ->
+  lists:map(
+    fun(#connection{value=V}) -> V end,
+    Connections).
+
+% calculates the output value of the perceptron based on the inputs
+% Uses the default sigmoid function as defined by sigmoid_fun/0
+output_value(Inputs) ->
+  output_value(sigmoid_fun(), Inputs).
+
+% caluculates the output value of the perceptron based on the inputs
+% Uses a user specified sigmoid function
+output_value(Func, Inputs) ->
+  Func(dot_prod(weights(Inputs), values(Inputs))).
+
+% Specifies the default sigmoid function
+sigmoid_fun() ->
+  fun(X) -> 1/(1+math:exp(-X)) end.
+
+% takes the dot product of two vectors, X and Y.
+dot_prod(X, Y) -> dot_prod(0, X, Y).
+
+dot_prod(Acc, [], []) -> Acc;
+dot_prod(Acc, [X1|X], [Y1|Y]) ->
+  dot_prod(X1*Y1 + Acc, X, Y).
